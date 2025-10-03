c303290cb24b59daa8c03cee927070226f6fd6ff
paigely <235818692+paigely@users.noreply.github.com>
2025-10-03 06:37:56 +0200

    shitcode

--- index.tsx
+++ index.tsx
@@ -4,24 +4,41 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import definePlugin from "@utils/types";
+import { definePluginSettings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+
+const replacement = {
+    match: /onClick:(\i\?void 0:\i)/,
+    replace: "$&,onMouseUp:$1"
+};
+
+const settings = definePluginSettings({
+    applyOnAllItems: {
+        type: OptionType.BOOLEAN,
+        description: "Whether to apply mouse up for all menu items, not just emojis",
+        default: false,
+        restartNeeded: true
+    }
+});
 
 export default definePlugin({
     name: "EmojiOnMouseUp",
     description: "Sends the emoji you are hovering when you take your mouse button up.",
+    settings,
     authors: [
+        Devs.sadan,
         {
-            name: "sadan",
-            id: 521819891141967883n
-        }
+            name: "perfect.symmetry",
+            id: 1375697625864601650n
+        },
     ],
     patches: [
+        { find: ".customItem;", replacement },
         {
-            find: ".customItem;",
-            replacement: {
-                match: /onClick:(\i\?void 0:\i)/,
-                replace: "$&,onMouseUp:$1"
-            }
+            find: ",subMenuIconClassName:",
+            replacement,
+            predicate: () => settings.store.applyOnAllItems
         }
     ]
 });
